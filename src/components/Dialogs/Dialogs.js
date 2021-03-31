c166cfc01064de5081caf28b0140d1955766b5ab
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-31 20:59:55 +0300

    24 feat: data BLL

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -4,19 +4,33 @@ import s from './Dialogs.module.css'
 import Message from './Message/Message'
 
 const Dialogs = () => {
+  let dialogData = [
+    { id: 1, name: 'Sveta' },
+    { id: 2, name: 'Sasha' },
+    { id: 3, name: 'Andrey' },
+    { id: 4, name: 'Misha' },
+    { id: 5, name: 'Max' },
+  ]
+
+  let messageData = [
+    { id: 1, message: 'Hello' },
+    { id: 2, message: 'this is test' },
+    { id: 3, message: '12345' },
+  ]
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <Dialog name="Sveta" id="1" />
-        <Dialog name="Sasha" id="2" />
-        <Dialog name="Andrey" id="3" />
-        <Dialog name="Misha" id="4" />
-        <Dialog name="Max" id="5" />
+        <Dialog name={dialogData[0].name} id={dialogData[0].id} />
+        <Dialog name={dialogData[1].name} id={dialogData[1].id} />
+        <Dialog name={dialogData[2].name} id={dialogData[2].id} />
+        <Dialog name={dialogData[3].name} id={dialogData[3].id} />
+        <Dialog name={dialogData[4].name} id={dialogData[4].id} />
       </div>
       <div className={s.dialogs__messages}>
-        <Message message="Hello" />
-        <Message message="this is test" />
-        <Message message="12345" />
+        <Message message={messageData[0].message} />
+        <Message message={messageData[1].message} />
+        <Message message={messageData[2].message} />
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 function MyPosts() {
+  let postData = [
+    { id: 1, message: 'Hello', like: 25 },
+    { id: 2, message: 'Its first post', like: 5 },
+    { id: 3, message: 'React JS', like: 2 },
+  ]
+
   return (
     <div>
       <div>My Posts</div>
@@ -11,9 +17,9 @@ function MyPosts() {
         <button>Add post</button>
       </div>
       <div className={s.posts}>
-        <Post message="Hello" like={5} />
-        <Post message="It's first post" like={2} />
-        <Post message="React JS" like={3} />
+        <Post message={postData[0].message} like={postData[0].like} />
+        <Post message={postData[1].message} like={postData[1].like} />
+        <Post message={postData[2].message} like={postData[2].like} />
       </div>
     </div>
   )
