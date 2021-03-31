fca4dba298eb793bfc213f1e66fe5739ac0c2e11
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-31 15:27:59 +0300

    22 feat: NavLink exact

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <div className={s.dialog}>Sveta</div>
-        <div className={s.dialog}>Sasha</div>
-        <div className={s.dialog + ' ' + s.active}>Andrey</div>
-        <div className={s.dialog}>Misha</div>
-        <div className={s.dialog}>Max</div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/1" activeClassName={s.active}>
+            Sveta
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2" activeClassName={s.active}>
+            Sasha
+          </NavLink>
+        </div>
+        <div className={s.dialog + ' ' + s.active}>
+          <NavLink to="/dialogs/3" activeClassName={s.active}>
+            Andrey
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4" activeClassName={s.active}>
+            Misha
+          </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5" activeClassName={s.active}>
+            Max
+          </NavLink>
+        </div>
       </div>
       <div className={s.dialogs__messages}>
         <div className={s.message}>Hello</div>
