052bdd4bd22aaee8fcc287d8f947418590e9dd87
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-31 15:00:34 +0300

    21 feat: add Dialogs, style active

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,19 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-    <div>
-      <div>Dialogs</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogs__items}>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Sasha</div>
+        <div className={s.dialog + ' ' + s.active}>Andrey</div>
+        <div className={s.dialog}>Misha</div>
+        <div className={s.dialog}>Max</div>
+      </div>
+      <div className={s.dialogs__messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>this is test</div>
+        <div className={s.message}>12345</div>
+      </div>
     </div>
   )
 }
