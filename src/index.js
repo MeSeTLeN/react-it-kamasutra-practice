a42a6458bed41ee4c66ccfd60dc3c2502be044fe
DeAdFrOnt <semenukmaks@gmail.com>
2021-04-03 11:46:58 +0300

    35 refactor: callback, pattern observer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import { rerenderEntireTree } from './render'
-import state from './redux/state'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import state, {
+  addMessage,
+  addPost,
+  updateNewMessageText,
+  updateNewPostText,
+  subscribe,
+} from './redux/state'
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+        addMessage={addMessage}
+        updateNewMessageText={updateNewMessageText}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
 
 rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree)
